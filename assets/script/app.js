3507444348a3dbb16452995679ccf4b2d2bf8062
saan1984 <sandeep_giet@yahoo.com>
2014-10-15 22:47:37 +0530

    changes

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,157 +1,181 @@
 'use strict';
-angular.module('profileApp',[
+angular.module('profileApp', [
     'ngRoute',
     'ngResource',
     'profileApp.profileController',
     'profileApp.profileServices'
 ])
+
     .config(['$routeProvider',
-        function($routeProvider) {
-            $routeProvider.when('/',{
+        function ($routeProvider) {
+            $routeProvider.when('/', {
                 templateUrl: 'view/profileTemplate.html',
                 controller: 'ProfileController'
             });
         }])
 
     .directive("responsiveImage",
-    ["$log","$window",
+    ["$log", "$window",
         function ($log, $window) {
             return {
-                restrict:'E',
-                replace:true,
+                restrict: 'E',
+                replace: true,
                 scope: {
                     'respalt': '@imagealt',
                     'respsrc': '@imagesrc'
                 },
                 template: '<img class="profile-image"' +
                     'ng-src="{{modifiedsrc}}" alt="{{respalt}}"/>',
-                link : function(scope, element, attribute){
-                    scope.width = $window.outerWidth;
-                    scope.$watch("width",function(newWidth,oldWidth){
-                        $log.log("New width of window : ",newWidth);
-                        if(newWidth <= 400){
-                            scope.modifiedsrc = scope.respsrc +"?s=80";
-                        }else if(newWidth >400 && newWidth <=767){
-                            scope.modifiedsrc = scope.respsrc +"?s=150";
-                        }else{
-                            scope.modifiedsrc = scope.respsrc +"?s=250";
-                        }
+                link: function (scope, element, attribute) {
+                    scope.$on("breakpointClassChange", function (event, argument) {
+                        $log.log("responsiveImage recieving breakpointClassChange ", argument);
+                        scope.$apply(function () {
+                            if (angular.equals(argument.styleClass, "large-screen")) {
+                                scope.modifiedsrc = scope.respsrc + "?s=250";
+                            } else if (angular.equals(argument.styleClass, "medium-screen")) {
+                                scope.modifiedsrc = scope.respsrc + "?s=150";
+                            } else if (angular.equals(argument.styleClass, "small-screen")) {
+                                scope.modifiedsrc = scope.respsrc + "?s=80";
+                            }
+                        })
+
                     });
-                    angular.element($window).bind('resize',function(){
-                        //Asking AngularJS to run digest cycle
-                        scope.$apply(function(){
-                            scope.width = $window.outerWidth;
+                }};
+        }])
+
+    .directive("responsiveHeader",
+    ["$log", "$window",
+        function ($log, $window) {
+            return {
+                restrict: 'E',
+                replace: true,
+                scope: {
+                    'respText': '@targettext'
+                },
+                template: "<p class='{{deviceSize}}'> {{respText}} </p>",
+                link: function (scope, element, attribute) {
+                    scope.$on("breakpointClassChange", function (event, argument) {
+                        $log.log("responsiveHeader recieving breakpointClassChange ", argument);
+                        scope.$apply(function () {
+                            scope.deviceSize = argument.styleClass;
                         })
                     });
                 }};
         }])
 
-.directive("responsiveHeader",
-    ["$log","$window",
-        function ($log, $window) {
+    .directive("responsiveParagraph",
+    ["$log", "$window", "$rootScope",
+        function ($log, $window, $rootScope) {
             return {
-                restrict:'E',
-                replace:true,
-                scope:{
-                    'respText':'@targettext'
-                },
-                template: "<p class='{{deviceSize}}'> {{respText}} </p>",
-                link : function(scope, element, attribute){
-                    scope.deviceSize = "largeDevice";
-                    scope.width = $window.outerWidth;
-                    scope.$watch("width",function(newWidth,oldWidth){
-                        if(newWidth <= 400){
-                            scope.deviceSize = "smallDevice"
-                        }else if(newWidth >400 && newWidth <=767){
-                            scope.deviceSize = "mediumDevice";
-                        }else{
-                            scope.deviceSize = "largeDevice";
-                        }
-                    });
-                    angular.element($window).bind('resize',function(){
-                        scope.$apply(function(){
-                            scope.width = $window.outerWidth;
-                        });
-                    });
-                }};
-        }])
-
-.directive("responsiveParagraph",
-    ["$log","$window",
-        function ($log, $window) {
-            return {
-                restrict:'E',
-                replace:true,
-                scope:{
-                    'respPara':'@targetpara'
+                restrict: 'E',
+                replace: true,
+                scope: {
+                    'respPara': '@targetpara'
                 },
                 template: "<p class='aboutme {{paragraphSize}}'> {{respPara}} </p>",
-                link : function(scope, element, attribute){
-                    scope.paragraphSize = "largePara";
-                    scope.width = $window.outerWidth;
-                    scope.$watch("width",function(newWidth,oldWidth){
-                        if(newWidth <= 400){
-                            scope.paragraphSize = "smallPara"
-                        }else if(newWidth >400 && newWidth <=767){
-                            scope.paragraphSize = "mediumPara";
-                        }else{
-                            scope.paragraphSize = "largePara";
-                        }
+                link: function (scope, element, attribute) {
+                    scope.$on("breakpointClassChange", function (event, argument) {
+                        $log.log("responsiveParagraph recieving breakpointClassChange ", argument);
+                        scope.$apply(function () {
+                            scope.paragraphSize = argument.styleClass;
+                        })
                     });
-                    angular.element($window).bind('resize',function(){
-                        scope.$apply(function(){
-                            scope.width = $window.outerWidth;
-                        });
-                    });
-               }};
+                }};
         }])
 
-.directive("responsiveList",
-    ["$log","$window",
+    .directive("breakpoint",
+    ["$log", "$window", "$rootScope", "$timeout",
+        function ($log, $window, $rootScope, $timeout) {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attributes) {
+                    var breakpointString = attributes.breakpoint,
+                        customBreakpoints = angular.fromJson(breakpointString);
+                    scope.breakpoint = {windowSize: $window.outerWidth, styleClass: ''};
+                    //Method for broadcast breakpointClassChange event
+                    scope.broadcastBreakEvent = function () {
+                        $log.log("Broadcasting breakpointClassChange...", scope.breakpoint);
+                        $timeout(function () {
+                            $rootScope.$broadcast('breakpointClassChange', scope.breakpoint);
+                        });
+                    }
+                    //Scope watcher for styleClass property to broadcast breakpointClassChange event
+                    scope.$watch('breakpoint.styleClass', function (newStyleClass, oldStyleClass) {
+                        if (newStyleClass.length > 0 && newStyleClass != oldStyleClass) {
+                            scope.broadcastBreakEvent();
+                        }
+                    });
+                    //Scope watcher for windowSize property to update the new style class
+                    scope.$watch('breakpoint.windowSize', function (newSize, oldSize) {
+                        var className = '';
+                        for (var customPointKey in customBreakpoints) {
+                            var breakSize = parseInt(customPointKey, 10);
+                            if (breakSize < newSize) {
+                                className = customBreakpoints[breakSize];
+                            }
+                        }
+                        scope.breakpoint.styleClass = className;
+                    });
+                    //Window resize event updates the windowSize property
+                    angular.element($window).bind('resize', function () {
+                        scope.$apply(function () {
+                            scope.breakpoint.windowSize = $window.outerWidth;
+                        });
+                    });
+                    //For first time page load
+                    angular.element(document).ready(function () {
+                        scope.broadcastBreakEvent();
+                    });
+                }
+            };
+        }])
+
+    .directive("responsiveList",
+    ["$log", "$window",
         function ($log, $window) {
             return {
-                restrict:'E',
-                replace:true,
-                scope:{
-                    'itemList':'=targetlist'
+                restrict: 'E',
+                replace: true,
+                scope: {
+                    'itemList': '=targetlist'
                 },
-                template: '<div class="item-list-container">'+
-                          '<ol>'+
-                          '<li ng-class="{smallText:isMorePresent}" ng-repeat="item in itemDisplayList">{{item}}</li>'+
-                          '</ol>'+
-                          '<button class="show-more" ng-show="isMorePresent" ' +
-                           'ng-click="showMore(itemDisplayList)"><span>More...</span></button>'+
-                          '</div>',
-                link : function(scope, element, attribute){
-                        scope.isMorePresent = false;
-
-                        scope.$watch("itemList",function(newItemList,oldItemList){
-                            scope.itemList = newItemList;
-                            scope.height = $window.outerHeight;
+                template: '<div class="item-list-container">' +
+                    '<ol>' +
+                    '<li ng-class="{smallText:isMorePresent}" ng-repeat="item in itemDisplayList">{{item}}</li>' +
+                    '</ol>' +
+                    '<button class="show-more" ng-show="isMorePresent" ' +
+                    'ng-click="showMore(itemDisplayList)"><span>More...</span></button>' +
+                    '</div>',
+                link: function (scope, element, attribute) {
+                    scope.isMorePresent = false;
+                    scope.$watch("itemList", function (newItemList, oldItemList) {
+                        scope.itemList = newItemList;
+                        scope.height = $window.outerHeight;
+                        scope.itemDisplayList = scope.itemList;
+                    }, true);
+                    scope.$watch("height", function (newHeight, oldHeight) {
+                        var listLength = angular.isDefined(scope.itemList) ?
+                            scope.itemList.length : 0;
+                        if (newHeight < 400 && listLength > 2) {
+                            scope.isMorePresent = true;
+                            scope.itemDisplayList = scope.itemList.slice(0, 2);
+                        } else if (newHeight >= 400 && newHeight < 700 && listLength > 3) {
+                            scope.isMorePresent = true;
+                            scope.itemDisplayList = scope.itemList.slice(0, 3);
+                        } else {
+                            scope.isMorePresent = false;
                             scope.itemDisplayList = scope.itemList;
-                        },true);
-                        scope.$watch("height",function(newHeight,oldHeight){
-                            var listLength = angular.isDefined(scope.itemList)?
-                                             scope.itemList.length : 0 ;
-                            if(newHeight <400 && listLength >2){
-                                scope.isMorePresent = true;
-                                scope.itemDisplayList = scope.itemList.slice(0,2);
-                            }else if(newHeight >= 400 && newHeight <700 && listLength >3){
-                                scope.isMorePresent = true;
-                                scope.itemDisplayList = scope.itemList.slice(0,3);
-                            }else{
-                                scope.isMorePresent = false;
-                                scope.itemDisplayList = scope.itemList;
-                            }
-                        });
-                        angular.element($window).bind('resize',function(){
-                            scope.$apply(function(){
-                                scope.height = $window.outerHeight;
-                            });
-                        });
-                        scope.showMore = function(initalList){
-                           scope.itemDisplayList = scope.itemList;
-                           scope.isMorePresent = false;
                         }
+                    });
+                    angular.element($window).bind('resize', function () {
+                        scope.$apply(function () {
+                            scope.height = $window.outerHeight;
+                        });
+                    });
+                    scope.showMore = function (initalList) {
+                        scope.itemDisplayList = scope.itemList;
+                        scope.isMorePresent = false;
+                    }
                 }};
         }]);
+
